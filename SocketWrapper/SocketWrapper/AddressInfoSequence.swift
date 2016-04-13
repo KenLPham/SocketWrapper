c7eea72d9119f3bfaa9d9757904d2867f08afd11
Marco Masser <marco.masser@obdev.at>
2016-04-13 18:36:56 +0200

    Moved initializer from to another class
    The initializer that calls `getaddrinfo` was defined on the internal, private `AddressInfoSequence.Storage` class, but it makes more sense as a private initializer on `AddressInfoSequence` itself. That way, the two classes’ responsibilities are better separated.

--- SocketWrapper/SocketWrapper/AddressInfoSequence.swift
+++ SocketWrapper/SocketWrapper/AddressInfoSequence.swift
@@ -25,50 +25,6 @@ struct AddressInfoSequence {
             _addrInfoPointer = addrInfoPointer
         }
 
-        /// Creates an instance by calling `getaddrinfo()` with the given arguments.
-        ///
-        /// Throws an error either if `getaddrinfo` failed, or if it returned an empty list. Therefore, if this initializer doesn't throw,
-        /// the created instance is guaranteed to have at least one `addrinfo`.
-        ///
-        /// - Parameter host: The hostname or IP address to resolve. Pass `nil` when resolving addresses for a server socket.
-        /// - Parameter port: The port or service name to resolve, e.g. "80" or "http". Pass `nil` when resolving an address
-        ///   for a protocol that does not use ports, e.g. ICMP.
-        /// - Parameter hints: A pointer to an `addrinfo` struct that is used as hints. This allows specifying things like
-        ///   the protocol family, socket type, or protocol. May be `nil`.
-        ///
-        /// - Important: `host` and `port` are both optional, but at least one of them must be given.
-        ///
-        /// - Note: See `getaddrinfo(3)` for more details about the parameters.
-        convenience init(host: String?, port: String?, hints: UnsafePointer<addrinfo>) throws {
-            var addrInfoPointer: UnsafeMutablePointer<addrinfo> = nil
-            let result: Int32
-
-            // `String` bridges to `UnsafePointer<Int8>` automatically, but `String?` does not. This
-            // switch takes care of the various combinations of `host` and `port` that can occur.
-            switch (host, port) {
-            case let (host?, port?):
-                result = getaddrinfo(host, port, hints, &addrInfoPointer)
-
-            case let (nil, port?):
-                result = getaddrinfo(nil, port, hints, &addrInfoPointer)
-
-            case let (host?, nil):
-                result = getaddrinfo(host, nil, hints, &addrInfoPointer)
-
-            default:
-                preconditionFailure("Either host or port must be given")
-            }
-
-            guard result != -1 else {
-                throw Socket.Error.GetAddrInfoFailed(code: result)
-            }
-            guard addrInfoPointer != nil else {
-                throw Socket.Error.NoAddressAvailable
-            }
-            
-            self.init(addrInfoPointer: addrInfoPointer)
-        }
-
         deinit {
             freeaddrinfo(_addrInfoPointer)
         }
@@ -98,16 +54,60 @@ struct AddressInfoSequence {
 
     /// Creates an instance for the given `host` and/or `port`.
     ///
-    /// - Note: See `getaddrinfo(3)` for more details about the parameters.
-    ///
-    /// - SeeAlso: `AddressInfoSequence.Storage`
+    /// - parameter family: For example `AF_UNSPEC`, i.e. IP4 or IP6.
+    /// - parameter socketType: For example `SOCK_STREAM`, i.e. a TCP socket.
+    /// - parameter flags: For example `AI_DEFAULT`.
     private init(host: String?, port: String?, family: Int32, socketType: Int32, flags: Int32) throws {
         var hints = addrinfo();
         hints.ai_family = family
         hints.ai_socktype = socketType
         hints.ai_flags = flags
 
-        _addrInfoStorage = try Storage(host: host, port: port, hints: &hints)
+        try self.init(host: host, port: port, hints: &hints)
+    }
+
+    /// Creates an instance by calling `getaddrinfo()` with the given arguments.
+    ///
+    /// Throws an error either if `getaddrinfo` failed, or if it returned an empty list. Therefore, if this initializer doesn't throw,
+    /// the created instance is guaranteed to have at least one `addrinfo`.
+    ///
+    /// - Parameter host: The hostname or IP address to resolve. Pass `nil` when resolving addresses for a server socket.
+    /// - Parameter port: The port or service name to resolve, e.g. "80" or "http". Pass `nil` when resolving an address
+    ///   for a protocol that does not use ports, e.g. ICMP.
+    /// - Parameter hints: A pointer to an `addrinfo` struct that is used as hints. This allows specifying things like
+    ///   the protocol family, socket type, or protocol. May be `nil`.
+    ///
+    /// - Important: `host` and `port` are both optional, but at least one of them must be given.
+    ///
+    /// - Note: See `getaddrinfo(3)` for more details about the parameters.
+    private init(host: String?, port: String?, hints: UnsafePointer<addrinfo>) throws {
+        var addrInfoPointer: UnsafeMutablePointer<addrinfo> = nil
+        let result: Int32
+
+        // `String` bridges to `UnsafePointer<Int8>` automatically, but `String?` does not. This
+        // switch takes care of the various combinations of `host` and `port` that can occur.
+        switch (host, port) {
+        case let (host?, port?):
+            result = getaddrinfo(host, port, hints, &addrInfoPointer)
+
+        case let (nil, port?):
+            result = getaddrinfo(nil, port, hints, &addrInfoPointer)
+
+        case let (host?, nil):
+            result = getaddrinfo(host, nil, hints, &addrInfoPointer)
+
+        default:
+            preconditionFailure("Either host or port must be given")
+        }
+
+        guard result != -1 else {
+            throw Socket.Error.GetAddrInfoFailed(code: result)
+        }
+        guard addrInfoPointer != nil else {
+            throw Socket.Error.NoAddressAvailable
+        }
+
+        _addrInfoStorage = Storage(addrInfoPointer: addrInfoPointer)
     }
 
 }
